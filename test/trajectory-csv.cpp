ab0c9ee1203e5166361ad01c4927cfda65ce0af2
Lars Berscheid <lars.berscheid@kit.edu>
2018-08-08 13:23:44 +0200

    modernize trajectory-csv

--- test/trajectory-csv.cpp
+++ test/trajectory-csv.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <cmath>
+#include <memory>
 
 #include <ReflexxesAPI.h>
 #include <RMLPositionFlags.h>
@@ -8,85 +9,49 @@
 #include <RMLPositionOutputParameters.h>
 
 
-#define CYCLE_TIME_IN_SECONDS                   0.001
-#define NUMBER_OF_DOFS                          1
+constexpr double CYCLE_TIME_IN_SECONDS {0.001};
+constexpr int NUMBER_OF_DOFS {1};
 
 
 int main() {
-  int                         ResultValue                 =   0       ;
-  ReflexxesAPI                *RML                        =   NULL    ;
-  RMLPositionInputParameters  *IP                         =   NULL    ;
-  RMLPositionOutputParameters *OP                         =   NULL    ;
-  RMLPositionFlags            Flags                                   ;
-
-  // ********************************************************************
-  // Creating all relevant objects of the Type II Reflexxes Motion Library
-
-  RML =   new ReflexxesAPI(NUMBER_OF_DOFS,   CYCLE_TIME_IN_SECONDS   );
-  IP  =   new RMLPositionInputParameters(     NUMBER_OF_DOFS          );
-  OP  =   new RMLPositionOutputParameters(    NUMBER_OF_DOFS          );
+  int ResultValue = 0;
+  auto RML = std::make_shared<ReflexxesAPI>(NUMBER_OF_DOFS, CYCLE_TIME_IN_SECONDS);
+  auto IP = std::make_shared<RMLPositionInputParameters>(NUMBER_OF_DOFS);
+  auto OP = std::make_shared<RMLPositionOutputParameters>(NUMBER_OF_DOFS);
+  RMLPositionFlags Flags;
 
 
-  IP->CurrentPositionVector->VecData      [0] =    0.0      ;
-//  IP->CurrentPositionVector->VecData      [1] =      0.0      ;
-//  IP->CurrentPositionVector->VecData      [2] =     50.0      ;
 
-  IP->CurrentVelocityVector->VecData      [0] =    0.0      ;
-//  IP->CurrentVelocityVector->VecData      [1] =   -220.0      ;
-//  IP->CurrentVelocityVector->VecData      [2] =    -50.0      ;
+  IP->CurrentPositionVector->VecData      [0] = 0.0;
+  IP->CurrentVelocityVector->VecData      [0] = 0.0;
+  IP->CurrentAccelerationVector->VecData  [0] = 0.0;
 
-  IP->CurrentAccelerationVector->VecData  [0] =   0.0      ;
-//  IP->CurrentAccelerationVector->VecData  [1] =    250.0      ;
-//  IP->CurrentAccelerationVector->VecData  [2] =    -50.0      ;
+  IP->MaxVelocityVector->VecData          [0] = 100.0;
+  IP->MaxAccelerationVector->VecData      [0] = 300.0;
 
-  IP->MaxVelocityVector->VecData          [0] =    100.0      ;
-//  IP->MaxVelocityVector->VecData          [1] =    100.0      ;
-//  IP->MaxVelocityVector->VecData          [2] =    300.0      ;
+  IP->TargetPositionVector->VecData       [0] = 49.0;
+  IP->TargetVelocityVector->VecData       [0] =  0.0;
 
-  IP->MaxAccelerationVector->VecData      [0] =    300.0      ;
-//  IP->MaxAccelerationVector->VecData      [1] =    200.0      ;
-//  IP->MaxAccelerationVector->VecData      [2] =    100.0      ;
+  IP->SelectionVector->VecData            [0] = true;
 
-//  IP->MaxJerkVector->VecData              [0] =    400.0      ;
-//  IP->MaxJerkVector->VecData              [1] =    300.0      ;
-//  IP->MaxJerkVector->VecData              [2] =    200.0      ;
 
-  IP->TargetPositionVector->VecData       [0] =   49.0      ;
-//  IP->TargetPositionVector->VecData       [1] =   -200.0      ;
-//  IP->TargetPositionVector->VecData       [2] =   -350.0      ;
-
-  IP->TargetVelocityVector->VecData       [0] =    0.0       ;
-//  IP->TargetVelocityVector->VecData       [1] =   -50.0       ;
-//  IP->TargetVelocityVector->VecData       [2] =  -200.0       ;
-
-  IP->SelectionVector->VecData            [0] =   true        ;
-//  IP->SelectionVector->VecData            [1] =   true        ;
-//  IP->SelectionVector->VecData            [2] =   true        ;
-
-  // ********************************************************************
-  // Starting the control loop
 
   std::ofstream outputFile("output.txt");
 
   while (ResultValue != ReflexxesAPI::RML_FINAL_STATE_REACHED) {
-    ResultValue =   RML->RMLPosition(*IP,   OP,   Flags       );
+    ResultValue = RML->RMLPosition(*IP, OP.get(), Flags);
+
+    outputFile << IP->CurrentPositionVector->VecData[0] << "," << IP->CurrentVelocityVector->VecData[0] << "," << IP->CurrentAccelerationVector->VecData[0] << std::endl;
 
     if (ResultValue < 0) {
       printf("An error occurred (%d).\n", ResultValue );
       break;
     }
 
-    *IP->CurrentPositionVector      =   *OP->NewPositionVector      ;
-    *IP->CurrentVelocityVector      =   *OP->NewVelocityVector      ;
-    *IP->CurrentAccelerationVector  =   *OP->NewAccelerationVector  ;
-
-    outputFile << IP->CurrentPositionVector->VecData[0] << "," << IP->CurrentVelocityVector->VecData[0] << "," << IP->CurrentAccelerationVector->VecData[0] << std::endl;
+    *IP->CurrentPositionVector = *OP->NewPositionVector;
+    *IP->CurrentVelocityVector  = *OP->NewVelocityVector;
+    *IP->CurrentAccelerationVector = *OP->NewAccelerationVector;
   }
-
-
-  delete  RML         ;
-  delete  IP          ;
-  delete  OP          ;
 
   exit(EXIT_SUCCESS)  ;
 }
